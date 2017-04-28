f9c8dcc070bc0072074a24c23278b78855c73930
AndreiCalazans <andreixoc@hotmail.com>
2017-04-28 09:02:46 -0300

    working on updating the graph, not finished

--- app/components/Main.js
+++ app/components/Main.js
@@ -2,18 +2,43 @@ import React from 'react';
 import 'jquery';
 
 
-// you need to parse the data coming from quandl API , the time must be in miliseconds
-// try to parse it before creating the chart.
-
-
 var seriesOptions = [],
     seriesCounter = 0,
-    // names = ['MSFT', 'AAPL', 'GOOG'];
-    names = ['ORB'];
+    names = ['MSFT', 'AAPL', 'GOOG', 'F'];
+
+
+// you need to update the graph when you update state
+// currently it is not updating instantly .
+// maybe you should pass the names as a value to the function.
+   
 
 
 class Main extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.state = {
+            names: ['MSFT', 'AAPL', 'GOOG', 'F']
+        }
+    }
 
+    handleSubmit(e) {
+        e.preventDefault();
+        console.log(this.refs.code.value);
+        let code = this.refs.code.value.toUpperCase();
+        this.setState((prevState , props) => {
+           
+            return {
+                names: [
+                    ...prevState.names,
+                    code
+                ]
+            }
+        })
+        
+        this.getData();
+
+    }
 
     createChart() {
 
@@ -54,14 +79,29 @@ class Main extends React.Component {
 }
    
     componentDidMount() {
-        let that = this;
-        $.each(names, function (i, name) {
-// https://www.quandl.com/api/v3/datasets/WIKI/FB.json?column_index=4collapse=daily&api_key=8TZgcVZUcVLzS2EUsioo
-    $.getJSON('https://www.quandl.com/api/v3/datasets/WIKI/'+name+'.json?column_index=4collapse=daily&api_key=8TZgcVZUcVLzS2EUsioo',    function (data) {
+        console.log('did');
+       this.getData();
+
+    }
+
+    getData() {
+        console.log('get data called', this.state.names);
+         let that = this;    
+        $.each(this.state.names , function (i, name) {
+// https://www.quandl.com/api/v3/datasets/WIKI/FB.json?column_index=4&order=asc&collapse=daily&api_key=8TZgcVZUcVLzS2EUsioo
+    $.getJSON('https://www.quandl.com/api/v3/datasets/WIKI/'+name+'.json?column_index=4&order=asc&collapse=daily&api_key=8TZgcVZUcVLzS2EUsioo',    function (data) {
+
+        let receivedData = data.dataset.data;
+        let parsedData = data.dataset.data.map((each)=> {
+            //below you are parsing the year-month-day format to milliseconds
+            return [ new Date(each[0]).getTime() ,each[1]]
+        })
+        
+        
 
         seriesOptions[i] = {
             name: data.dataset.name,
-            data: data.dataset.data
+            data: parsedData
         };
 
         // As we're loading the data asynchronously, we don't know what order it will arrive. So
@@ -73,7 +113,6 @@ class Main extends React.Component {
         }
     });
 });
-
     }
 
     render() {
@@ -82,6 +121,15 @@ class Main extends React.Component {
             <div>
                 <h1>Stock Chart</h1>
                 <div id="container" style={{height: "400px", minWidth: "310px"}}></div>
+                <div>
+                    <form onSubmit={this.handleSubmit} >
+                        <label htmlFor="">
+                            Follow a Stock, Add a Stock Code: 
+                            <input type="text" ref='code' />
+                            <button>Add</button>
+                        </label>
+                    </form>
+                </div>
             </div>
         )
     }
