236cf5fbba164b9a35c3e726935ff4f74d82a615
AndreiCalazans <andreixoc@hotmail.com>
2017-05-01 17:53:59 -0300

    preparing for production

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
-var bodyParser= require('body-parser');
 var app = express(); 
 
 var PORT = process.env.PORT || 3000;
@@ -14,15 +13,9 @@ var io = require('socket.io')(server);
 
 
 
-var mongoose = require('mongoose');
-require('dotenv').config();
 
-// https://www.quandl.com/api/v3/datasets/WIKI/FB.json?api_key=8TZgcVZUcVLzS2EUsioo
-// app.use(favicon(path.join(__dirname, 'dist/img', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'dist/img', 'favicon.ico')));
 
-
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
 
 
 app.use(express.static('dist'));
@@ -35,7 +28,7 @@ app.get('*.js', function (req, res, next) {
 
 var codes = ['MSFT', 'AAPL', 'GOOG'  ];
 io.on('connection', function(socket) {
-  console.log('a user connect');
+  
   
   
   io.emit('update', codes);
@@ -45,19 +38,16 @@ io.on('connection', function(socket) {
     // an if statement to impede trying to delete something that doesnt exist
     if(codes.indexOf(stock) >= 0) {
       codes.splice(codes.indexOf(stock), 1);
-      console.log('remove', codes);
+      
       io.emit('update', codes);
     }
 
   })
   socket.on('addStock' , function(stock) {
     codes.push(stock);
-    console.log(codes);
+    
     io.emit('update', codes);
 })
-  socket.on('new', function(msg) {
-    console.log(msg);
-  })
 
   socket.on('disconnect' , function() {
     console.log('user disconnected');
--- webpack.config.js
+++ webpack.config.js
@@ -49,22 +49,22 @@ module.exports = {
     new webpack.ProvidePlugin({
       $: 'jquery',
       jQuery: 'jquery'
+    }),
+     new webpack.DefinePlugin({ // <-- key to reducing React's size
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin(), //minify everything
+    new webpack.optimize.AggressiveMergingPlugin(),//Merge chunks 
+     new CompressionPlugin({ 
+      asset: "[path].gz[query]",
+      algorithm: "gzip",
+      test: /\.js$|\.css$|\.html$/,
+      threshold: 10240,
+      minRatio: 0.8
     })
-    //  new webpack.DefinePlugin({ // <-- key to reducing React's size
-    //   'process.env': {
-    //     'NODE_ENV': JSON.stringify('production')
-    //   }
-    // })
-    // new webpack.optimize.UglifyJsPlugin(), //minify everything
-    // new webpack.optimize.AggressiveMergingPlugin(),//Merge chunks 
-    //  new CompressionPlugin({ 
-    //   asset: "[path].gz[query]",
-    //   algorithm: "gzip",
-    //   test: /\.js$|\.css$|\.html$/,
-    //   threshold: 10240,
-    //   minRatio: 0.8
-    // })
 
-  ],
-devtool: 'cheap-module-eval-source-map'
+  ]
+// devtool: 'cheap-module-eval-source-map'
 }
